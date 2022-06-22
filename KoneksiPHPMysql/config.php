43ca1568a8e0907b86ef1781afe9039155a51060
debiapriani22 <105537067+debiapriani22@users.noreply.github.com>
2022-06-22 14:47:49 +0700

    Add files via upload

---
+++ KoneksiPHPMysql/config.php
@@ -0,0 +1,15 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'crud_db';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+//mengecek koneksi
+if (!$mysqli) {
+	die("Koneksi gagal :" . mysqli_connect_error());
+}
+echo "Koneksi berhasil";
+mysqli_close($mysqli);
+?>
